cfe025c475c15e80cbc54807615a45513bcd5c08
Abhinav Verma <abhinavverma658@gmail.com>
2023-06-08 00:41:01 +0530

    changes

--- src/Into_OOPS_2.java
+++ src/Into_OOPS_2.java
@@ -1,3 +1,7 @@
+/*
+Q2. Write a program to print the area and perimeter of a triangle having sides of 3, 4 and 5 units by
+creating a class named 'Triangle' without any parameter in its constructor.
+ */
 import java.util.Scanner;
 
 class triangle {
