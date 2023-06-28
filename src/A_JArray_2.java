cd62c4aece279bfc8c3996e74bd495b6d4f03796
Abhinav Verma <abhinavverma658@gmail.com>
2023-06-28 10:02:45 +0530

    changes

---
+++ src/A_JArray_2.java
@@ -0,0 +1,12 @@
+//Q2. Convert the list of Strings {“ab”, “bc”, “cd”, “de”, “ef”, “fg”, “gh”} into an array of strings and print all
+//      strings stored on odd indices of the array.
+import java.util.Scanner;
+public class A_JArray_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String arr[] ={"ab","bc","cd","de","ef","fg","gh"};
+        for(int i=0;i< arr.length;i++){
+            if(i%2!=0) System.out.println(arr[i]);
+        }
+    }
+}
